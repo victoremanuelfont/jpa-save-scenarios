515329a1c6bb8df3bbbf3160b1f16d807c83a554
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-01-27 14:33:15 -0300

    feat: criação e mapeamento das entidades Person e Department

---
+++ src/main/java/com/ufc/estudo/entities/Person.java
@@ -0,0 +1,52 @@
+package com.ufc.estudo.entities;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private Double salary;
+
+    private Department department;
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, Double salary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
